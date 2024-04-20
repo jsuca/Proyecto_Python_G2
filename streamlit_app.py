1c80041662e944cb91d1c96c7e70ba85edc7fc23
jsuca <47945622+jsuca@users.noreply.github.com>
2024-04-19 22:34:54 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from shapely.geometry import Point
 import geopandas as gpd
 from geopandas import GeoDataFrame
+import random
 
 @st.cache_data
 def load_data_vacunacion(url_vacunacion, url_ubigeo):
@@ -26,16 +27,39 @@ df_geo = load_data_vacunacion("TB_CENTRO_VACUNACION.csv", "TB_UBIGEOS.csv")
 
 st.title('Centros de Vacunación de COVID 19')
 
-option = st.selectbox('Elegir departamento:', ('APURIMAC', 'HUANCAVELICA', 'CUSCO', 'ANCASH', 'LORETO', 'HUANUCO',
-       'AREQUIPA', 'LA LIBERTAD', 'UCAYALI', 'PIURA', 'PUNO', 'AMAZONAS',
-       'LIMA', 'SAN MARTIN', 'CAJAMARCA', 'CALLAO', 'TACNA', 'AYACUCHO',
-       'JUNIN', 'LAMBAYEQUE', 'ICA', 'TUMBES', 'PASCO', 'MOQUEGUA',
-       'MADRE DE DIOS'))
+departamentos = df_geo['departamento'].unique()
+departamentos_seleccionados = st.multiselect('Selecciona los departamentos:', departamentos)
 
-st.write('Departamento seleccionado:', option)
+if not departamentos_seleccionados:
+    st.write("No se han seleccionado departamentos.")
+else:
+    df_seleccionado = df_geo[df_geo['departamento'].isin(departamentos_seleccionados)]
 
-fig, ax = plt.subplots(figsize = (10,8))
-region_geojson[region_geojson.FIRST_NOMB == option].plot(ax = ax, edgecolor=u'gray', cmap='Pastel1')
-df_geo[df_geo['departamento'] == option].plot(ax = ax, color = 'black', markersize=2)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.set_aspect('equal')
+    
+    region_geojson[region_geojson.FIRST_NOMB.isin(departamentos_seleccionados)].plot(ax=ax, edgecolor=u'gray', cmap='Pastel1')
+    df_seleccionado.plot(ax=ax, color='black', markersize=2)
+    st.pyplot(fig)
+    
+    departamentos_unicos = df_seleccionado['departamento'].unique()
+    random.seed(89040)
+    colors_bar = {}
+    for departamento in departamentos_unicos:
+        color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+        colors_bar[departamento] = color
+    
+    provincias_counts = df_seleccionado['provincia'].value_counts()
+    
+    fig_bar, ax_bar = plt.subplots()
+    provincias_counts.plot(kind='bar', ax=ax_bar, color=[colors_bar[df_seleccionado.loc[df_seleccionado['provincia'] == provincia, 'departamento'].iloc[0]] for provincia in provincias_counts.index])
+    ax_bar.set_xlabel('Provincia')
+    ax_bar.set_ylabel('Cantidad')
+    ax_bar.set_title('Cantidad de Centros de Vacunación por provincia')
+    
+    legend_handles = []
+    for departamento, color in colors_bar.items():
+        legend_handles.append(matplotlib.patches.Patch(color=color, label=departamento))
+        ax_bar.legend(handles=legend_handles, title="Departamentos")
 
-st.pyplot(fig)
+    st.pyplot(fig_bar)
